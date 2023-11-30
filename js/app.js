e5e8916bfbe1e314cc3112262880931c634fba11
AJ Harris <shogunaut90@gmail.com>
2023-11-29 21:45:38 -0600

    Added functionality to remove workout or meal from DOM

--- js/app.js
+++ js/app.js
@@ -30,6 +30,26 @@ class CalorieTracker {
     this._render();
   }
 
+  removeMeal(id) {
+    const index = this._meals.findIndex((meal) => meal.id === id);
+    if(index !== -1) {
+      const meal = this._meals[index];
+      this._totalCalories -= meal.calories;
+      this._meals.splice(index, 1);
+      this._render();
+    }
+  }
+
+  removeWorkout(id) {
+    const index = this._workouts.findIndex((workout) => workout.id === id);
+    if(index !== -1) {
+      const workout = this._workouts[index];
+      this._totalCalories += workout.calories;
+      this._workouts.splice(index, 1);
+      this._render();
+    }
+  }
+
   // PRIVATE METHODS
   // Display total Calories
   _displayCaloriesTotal() {
@@ -168,6 +188,8 @@ class App {
     this._tracker = new CalorieTracker();
     document.getElementById('meal-form').addEventListener('submit', this._newItem.bind(this, 'meal'));
     document.getElementById('workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'));  
+    document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this, 'meal'));
+    document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'));
   }
 
   _newItem(type, e) {
@@ -203,6 +225,18 @@ class App {
       toggle: true
     })
     // console.log(this);
+  }
+
+  _removeItem(type, e) {
+    if(e.target.classList.contains('delete') || e.target.classList.contains('fa-xmark')) {
+      if(confirm('Are you sure you want to remove this item?')) {
+        // console.log('deleted');
+        const id = e.target.closest('.card').getAttribute('data-id');
+        type === 'meal' ? this._tracker.removeMeal(id) : this._tracker.removeWorkout(id);
+        e.target.closest('.card').remove();
+        // console.log(id);
+      }
+    }
   }
 }
 
