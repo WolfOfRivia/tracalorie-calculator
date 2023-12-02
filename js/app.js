dea729e332e68ff86714de02b720738ca3214330
AJ Harris <shogunaut90@gmail.com>
2023-12-01 18:18:11 -0600

    Added set daily calorie limit functionality

--- js/app.js
+++ js/app.js
@@ -58,6 +58,12 @@ class CalorieTracker {
     this._render();
   }
 
+  setLimit(calorieLimit) {
+    this._calorieLimit = calorieLimit;
+    this._displayCalorieLimit();
+    this._render();
+  }
+
   // PRIVATE METHODS
   // Display total Calories
   _displayCaloriesTotal() {
@@ -201,6 +207,7 @@ class App {
     document.getElementById('filter-meals').addEventListener('keyup', this._filterItems.bind(this, 'meal'));
     document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'));
     document.getElementById('reset').addEventListener('click', this._reset.bind(this));
+    document.getElementById('limit-form').addEventListener('submit', this._setLimit.bind(this));
   }
 
   _newItem(type, e) {
@@ -268,6 +275,22 @@ class App {
     document.getElementById('workout-items').innerHTML = '';
     document.getElementById('filter-meals').value = '';
     document.getElementById('filter-workouts').value = '';
+  }
+
+  _setLimit(e) {
+    e.preventDefault();
+    const limit = document.getElementById('limit');
+    if(limit.value === '') {
+      alert('Please enter a limit.');
+      return;
+    }
+
+    this._tracker.setLimit(+limit.value);
+    limit.value = '';
+
+    const modalEl = document.getElementById('limit-modal');
+    const modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
   }
 }
 
